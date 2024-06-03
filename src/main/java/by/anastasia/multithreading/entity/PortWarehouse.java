ab75cef3bd7600fa1aabe583557395478c923e32
anasteish22 <fr.anasteish@gmail.com>
2024-06-03 17:45:25 +0300

    .

--- src/main/java/by/anastasia/multithreading/entity/PortWarehouse.java
+++ src/main/java/by/anastasia/multithreading/entity/PortWarehouse.java
@@ -3,10 +3,14 @@ package by.anastasia.multithreading.entity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PortWarehouse {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final PortWarehouse instance = new PortWarehouse();
     private int maxCapacity;
+    private List<Pier> piers = new ArrayList<>();
 
     public static PortWarehouse getInstance() {
         return instance;
